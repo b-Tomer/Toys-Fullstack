68c59c09b6a017084789922c4efa0eaac201e896
T0mer007 <tomerbenaim1@gmail.com>
2023-05-27 14:11:32 +0300

    third touch

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,15 @@ const cookieParser = require('cookie-parser')
 const path = require('path')
 const cors = require('cors')
 
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')))
-// } else {
-//     const corsOptions = {
-//         origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
-//         credentials: true
-//     }
-//     app.use(cors(corsOptions))
-// }
+} else {
+    const corsOptions = {
+        origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
+}
 
 
 // App Configuration
